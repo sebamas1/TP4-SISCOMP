760dfbec5a605e795bcd938ccc9a7d925e58416b
Sebastian <sebamas1@hotmail.com>
2022-06-09 04:08:49 +0000

    feat: ahora si togglea el led cuando apretas el pulsador

--- modulo.c
+++ modulo.c
@@ -8,12 +8,11 @@
 #include <linux/cdev.h>
 #include <linux/delay.h>
 #include <linux/slab.h>
+#include <linux/gpio.h>
 
 static dev_t first;       // Global variable for the first device number
 static struct cdev c_dev; // Global variable for the character device structure
 static struct class *cl;  // Global variable for the device class
-
-static char c;
 
 static int my_open(struct inode *i, struct file *f)
 {
@@ -31,160 +30,78 @@ static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off
     printk(KERN_INFO "TP_FINAL: read()\n");
     return 0;
 }
+/*
+Configura e inicializa el pin pasado como argumento(string). Toma como argumento in o out
+que es la direccion del pin. Si es in, lo configura como entrada, si es out, como salida.
+*/
+static void inicializar_pin(char *pin, char *in_out)
+{
+    if (strcmp(in_out, "in") == 0)
+    {
+        printk(KERN_INFO "TP_FINAL: Configurando pin %s como entrada\n", pin);
+        gpio_direction_input(simple_strtoul(pin, NULL, 10));
+    }
+    else if (strcmp(in_out, "out") == 0)
+    {
+        printk(KERN_INFO "TP_FINAL: Configurando pin %s como salida\n", pin);
+        gpio_direction_output(simple_strtoul(pin, NULL, 10), 0);
+    }
+    else
+    {
+        printk(KERN_INFO "TP_FINAL: Error en la configuracion del pin %s\n", pin);
+    }
+}
+/*
+Setea el pin al valor pasado como argumento(string). Toma como argumento el pin y el valor.
+*/
+static void setear_pin(char *pin, char *valor)
+{
+    if (strcmp(valor, "1") == 0)
+    {
+        printk(KERN_INFO "TP_FINAL: Seteando pin %s a 1\n", pin);
+        gpio_set_value(simple_strtoul(pin, NULL, 10), 1);
+    }
+    else if (strcmp(valor, "0") == 0)
+    {
+        printk(KERN_INFO "TP_FINAL: Seteando pin %s a 0\n", pin);
+        gpio_set_value(simple_strtoul(pin, NULL, 10), 0);
+    }
+    else
+    {
+        printk(KERN_INFO "TP_FINAL: Error en el seteo del pin %s\n", pin);
+    }
+}
+/*
+Lee el pin pasado como argumento(string). Toma como argumento el pin.
+*/
+static char leer_pin(char *pin)
+{
+    char res;
+    printk(KERN_INFO "TP_FINAL: Leyendo pin %s\n", pin);
+    res = gpio_get_value(simple_strtoul(pin, NULL, 10));
+    return res;
+}
 
 static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
 {
-    loff_t pos;
-
-    // Export the desired pin by writing to /sys/class/gpio/export
-    struct file *fd = filp_open("/sys/class/gpio/export", O_WRONLY, 0644);
-    if (IS_ERR(fd))
-    {
-        printk(KERN_INFO "TP_FINAL: Error opening /sys/class/gpio/export\n");
-        return PTR_ERR(fd);
-    }
-    char *gpio_num = "24";
-    kernel_write(fd, gpio_num, strlen(gpio_num), &pos);
-    if (IS_ERR(fd))
-    {
-        printk(KERN_INFO "TP_FINAL: Error writing to /sys/class/gpio/export\n");
-        return PTR_ERR(fd);
-    }
-    filp_close(fd, NULL);
-
-    // Set the pin to be an output by writing "out" to /sys/class/gpio/gpio24/direction
-    fd = filp_open("/sys/class/gpio/gpio24/direction", O_WRONLY, 0644);
-    if (IS_ERR(fd))
-    {
-        printk(KERN_INFO "TP_FINAL: Error opening /sys/class/gpio/gpio24/direction\n");
-        return PTR_ERR(fd);
-    }
-    char *gpio_dir = "out";
-    kernel_write(fd, gpio_dir, strlen(gpio_dir), &pos);
-    if (IS_ERR(fd))
-    {
-        printk(KERN_INFO "TP_FINAL: Error writing to /sys/class/gpio/gpio24/direction\n");
-        return PTR_ERR(fd);
-    }
-    filp_close(fd, NULL);
-
-    fd = filp_open("/sys/class/gpio/export", O_WRONLY, 0644);
-    if (IS_ERR(fd))
-    {
-        printk(KERN_INFO "TP_FINAL: Error opening /sys/class/gpio/export\n");
-        return PTR_ERR(fd);
-    }
-    gpio_num = "23";
-    kernel_write(fd, gpio_num, strlen(gpio_num), &pos);
-    if (IS_ERR(fd))
-    {
-        printk(KERN_INFO "TP_FINAL: Error writing to /sys/class/gpio/export\n");
-        return PTR_ERR(fd);
-    }
-    filp_close(fd, NULL);
-
-    // Set the pin to be an output by writing "out" to /sys/class/gpio/gpio24/direction
-    fd = filp_open("/sys/class/gpio/gpio23/direction", O_WRONLY, 0644);
-    if (IS_ERR(fd))
-    {
-        printk(KERN_INFO "TP_FINAL: Error opening /sys/class/gpio/gpio23/direction\n");
-        return PTR_ERR(fd);
-    }
-    gpio_dir = "in";
-    kernel_write(fd, gpio_dir, strlen(gpio_dir), &pos);
-    if (IS_ERR(fd))
-    {
-        printk(KERN_INFO "TP_FINAL: Error writing to /sys/class/gpio/gpio23/direction\n");
-        return PTR_ERR(fd);
-    }
-    filp_close(fd, NULL);
-
-        char *gpio_val;
-        // aloca memoria para leer el valor del pin
-        gpio_val = kmalloc(sizeof(char), GFP_KERNEL);
-
     int i = 0;
-    while (i < 10)
+    int aux = 30;
+    inicializar_pin("24", "out");
+    setear_pin("24", "1");
+    inicializar_pin("23", "in");
+    while (i < aux)
     {
-        fd = filp_open("/sys/class/gpio/gpio23/value", O_RDONLY, 0644);
-        if (IS_ERR(fd))
+        if (leer_pin("23") == 1)
         {
-            printk(KERN_INFO "TP_FINAL: Error opening /sys/class/gpio/gpio23/value\n");
-            return PTR_ERR(fd);
-        }
-
-        loff_t hola = 0;
-        kernel_read(fd, gpio_val, 1, &hola);
-        if (IS_ERR(fd))
-        {
-            printk(KERN_INFO "TP_FINAL: Error reading from /sys/class/gpio/gpio23/value\n");
-            return PTR_ERR(fd);
-        }
-        printk(KERN_INFO "TP_FINAL: gpio_val = %d\n", *gpio_val);
-
-        filp_close(fd, NULL);
-        if (gpio_val[0] == '0')
-        {
-            // Set the pin to be high by writing "1" to /sys/class/gpio/gpio24/value
-            fd = filp_open("/sys/class/gpio/gpio24/value", O_WRONLY, 0644);
-
-            if (IS_ERR(fd))
-            {
-                printk(KERN_INFO "TP_FINAL: Error opening /sys/class/gpio/gpio24/value\n");
-                return PTR_ERR(fd);
-            }
-            char *gpio_val = "1";
-
-            kernel_write(fd, gpio_val, strlen(gpio_val), &pos);
-            if (IS_ERR(fd))
-            {
-                printk(KERN_INFO "TP_FINAL: Error writing to /sys/class/gpio/gpio24/value\n");
-                return PTR_ERR(fd);
-            }
-            filp_close(fd, NULL);
-	    printk(KERN_INFO "DEBERIA PRENDER\n");
+            setear_pin("24", "0");
         }
         else
         {
-            // Set the pin to be high by writing "1" to /sys/class/gpio/gpio24/value
-            fd = filp_open("/sys/class/gpio/gpio24/value", O_WRONLY, 0644);
-
-            if (IS_ERR(fd))
-            {
-                printk(KERN_INFO "TP_FINAL: Error opening /sys/class/gpio/gpio24/value\n");
-                return PTR_ERR(fd);
-            }
-            char *gpio_val = "0";
-
-            kernel_write(fd, gpio_val, strlen(gpio_val), &pos);
-            if (IS_ERR(fd))
-            {
-                printk(KERN_INFO "TP_FINAL: Error writing to /sys/class/gpio/gpio24/value\n");
-                return PTR_ERR(fd);
-            }
-            filp_close(fd, NULL);
-	    printk(KERN_INFO "DEBERIA APAGAR\n");
+            setear_pin("24", "1");
         }
-        msleep(1500);
-        i++;
-	memset(gpio_val, 0, sizeof(gpio_val));
+        msleep(1000);
+	i++;
     }
-
-    // Unexport the pin by writing to /sys/class/gpio/unexport
-    fd = filp_open("/sys/class/gpio/unexport", O_WRONLY, 0644);
-    if (IS_ERR(fd))
-    {
-        printk(KERN_INFO "TP_FINAL: Error opening /sys/class/gpio/unexport\n");
-        return PTR_ERR(fd);
-    }
-    char *gpio_unexport = "24";
-    kernel_write(fd, gpio_unexport, strlen(gpio_unexport), &pos);
-    if (IS_ERR(fd))
-    {
-        printk(KERN_INFO "TP_FINAL: Error writing to /sys/class/gpio/unexport\n");
-        return PTR_ERR(fd);
-    }
-    filp_close(fd, NULL);
 
     printk(KERN_INFO "finalizando write\n");
     // And exit
