2487baf5522448989a4bfc431f99dfca798c8051
nehemias <nehemias.mercau@mi.unc.edu.ar>
2022-06-13 20:42:04 -0300

    colores de graficos

--- leertemperatura.py
+++ leertemperatura.py
@@ -5,7 +5,7 @@ import subprocess
 Y = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 while(True):
 
-    contenido = subprocess.run(['cat', 'temperaturas.txt'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+    contenido = subprocess.run(['cat', '/dev/TP_FINAL'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
     X = np.asarray(contenido.split(','))
     substring = "T"
     if substring in X[0]:
@@ -14,7 +14,9 @@ while(True):
         plt.title('Temperatura de clima en 10 días')
         plt.xlabel('Muestra N°')
         plt.ylabel('Temperatura [°C]')
-        plt.bar(Y, X)
+        plt.plot(Y, X, 'o-')
+        plt.fill_between(Y, X, color='blue', alpha=0.5)
+        
         plt.draw()
         plt.pause(0.0001)
         plt.clf()
@@ -25,7 +27,8 @@ while(True):
         plt.title('Caudal de un río en 10 días')
         plt.xlabel('Muestra N°')
         plt.ylabel('Caudal [m3/s]')
-        plt.bar(Y, X)
+        plt.plot(Y, X, 'go')
+        plt.fill_between(Y, X, color='green', alpha=0.7)
         plt.draw()
         plt.pause(0.0001)
         plt.clf()
